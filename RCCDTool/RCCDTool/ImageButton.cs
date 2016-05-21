68cdc891118c529e247f606bc20dea7b3f3e725b
rbudnar <budnarr@gmail.com>
2016-05-20 21:46:20 -0500

    Test commit for pivotal.

---
+++ RCCDTool/RCCDTool/ImageButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace RCCDTool
+{
+    class ImageButton : Button
+    {
+        public ImageSource Source
+        {
+            get { return base.GetValue(SourceProperty) as ImageSource; }
+            set { base.SetValue(SourceProperty, value);}
+        }
+
+        public static readonly DependencyProperty SourceProperty = DependencyProperty.Register("Source",
+            typeof (ImageSource), typeof (ImageButton));
+    }
+}
